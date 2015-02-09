989fc6d1c9e24cda6379639ac530f15d666fa92c
Supremicus <Supremicus@users.noreply.github.com>
2015-02-10 00:39:06 +1000

    Remove kickmirror from KickAssTorrents
    * Remove kickmirror from KickAssTorrents as it is no longer available
    * Change KickAssTorrents url from .so to .to

--- sickbeard/providers/kat.py
+++ sickbeard/providers/kat.py
@@ -61,8 +61,8 @@ class KATProvider(generic.TorrentProvider):
 
         self.cache = KATCache(self)
 
-        self.urls = ['http://kickass.so/', 'http://katproxy.com/', 'http://www.kickmirror.com/']
-        self.url = 'https://kickass.so/'
+        self.urls = ['http://kickass.to/', 'http://katproxy.com/']
+        self.url = 'https://kickass.to/'
 
     def isEnabled(self):
         return self.enabled
