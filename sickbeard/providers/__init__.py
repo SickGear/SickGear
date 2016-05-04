06fec4db070b24e0d4e69bb1794ed888ef826282
JackDandy <jackdandy@users.noreply.github.com>
2016-05-04 16:22:13 +0100

    Add provider Anizb.

--- sickbeard/providers/__init__.py
+++ sickbeard/providers/__init__.py
@@ -26,7 +26,7 @@ from sickbeard import logger, encodingKludge as ek
 # usenet
 from . import newznab, omgwtfnzbs, womble
 # torrent
-from . import alpharatio, beyondhd, bitmetv, btn, filelist, freshontv, funfile, gftracker, grabtheinfo, \
+from . import alpharatio, anizb, beyondhd, bitmetv, btn, filelist, freshontv, funfile, gftracker, grabtheinfo, \
     hd4free, hdbits, hdspace, iptorrents, kat, morethan, pisexy, pretome, rarbg, scc, scenetime, shazbat, speedcd, \
     thepiratebay, torrentbytes, torrentday, torrenting, torrentleech, torrentshack, transmithe_net, tvchaosuk
 # anime
@@ -40,6 +40,7 @@ except:
 __all__ = ['omgwtfnzbs',
            'womble',
            'alpharatio',
+           'anizb',
            'beyondhd',
            'bitmetv',
            'btn',
