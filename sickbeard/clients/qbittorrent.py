b8ddf97b119e318019b5351c5871f743a5a2fcd9
JackDandy <jackdandy@users.noreply.github.com>
2017-11-01 12:19:09 +0000

    Change qBittorent to handle the change to its API success/fail response.
    qB changed how it responds to API action from '' to 'Ok.' and 'Fails.', therefore breaking apps.

--- sickbeard/clients/qbittorrent.py
+++ sickbeard/clients/qbittorrent.py
@@ -39,7 +39,7 @@ class QbittorrentAPI(GenericClient):
 
     def _post_api(self, cmd='', **kwargs):
 
-        return '' == helpers.getURL('%scommand/%s' % (self.host, cmd), session=self.session, **kwargs)
+        return helpers.getURL('%scommand/%s' % (self.host, cmd), session=self.session, **kwargs) in ('', 'Ok.')
 
     def _add_torrent(self, cmd, **kwargs):
 
