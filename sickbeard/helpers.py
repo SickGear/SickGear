a730f41d4680de6db49b8332b6220c0eaafc7cb9
JackDandy <jackdandy@users.noreply.github.com>
2018-01-15 01:42:24 +0000

    Add 'PB', 'EB', 'ZB', 'YB' to recognised output sizes.

--- sickbeard/helpers.py
+++ sickbeard/helpers.py
@@ -275,7 +275,7 @@ def searchIndexerForShowID(regShowName, indexer=None, indexer_id=None, ui=None):
 
 
 def sizeof_fmt(num):
-    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']:
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
         num /= 1024.0
