102fadd61369870c561ae089ed65f7410f6c7ebc
JackDandy <jackdandy@users.noreply.github.com>
2020-10-19 10:14:11 +0100

    Add `autoProcessTV/sabToSickGear.py` that works with SABnzbd under both py2 and py3.

---
+++ autoProcessTV/sabToSickGear.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Author: Nic Wolfe <nic@wolfeden.ca>
+# URL: http://code.google.com/p/sickbeard/
+#
+# This file is part of SickGear.
+#
+# SickGear is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickGear is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with SickGear.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from __future__ import print_function
+import sys
+import autoProcessTV
+
+if len(sys.argv) < 2:
+    print('No folder supplied - is this being called from SABnzbd?')
+    sys.exit()
+elif len(sys.argv) >= 8:
+    autoProcessTV.process_files(sys.argv[1], sys.argv[2], sys.argv[7])
+elif len(sys.argv) >= 3:
+    autoProcessTV.process_files(sys.argv[1], sys.argv[2])
+else:
+    autoProcessTV.process_files(sys.argv[1])
