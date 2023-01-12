a02bc55069b523c2b81ac1f6d8548ba57bf84d0b
JackDandy <jackdandy@users.noreply.github.com>
2023-01-12 22:37:09 +0000

    Update Send2Trash 1.5.0 (66afce7) → 1.8.1b0 (0ef9b32).

--- lib/send2trash/mac/legacy.py
+++ lib/send2trash/mac/legacy.py
@@ -9,10 +9,11 @@ from __future__ import unicode_literals
 from ctypes import cdll, byref, Structure, c_char, c_char_p
 from ctypes.util import find_library
 
-from .compat import binary_type
+from send2trash.compat import binary_type
+from send2trash.util import preprocess_paths
 
-Foundation = cdll.LoadLibrary(find_library('Foundation'))
-CoreServices = cdll.LoadLibrary(find_library('CoreServices'))
+Foundation = cdll.LoadLibrary(find_library("Foundation"))
+CoreServices = cdll.LoadLibrary(find_library("CoreServices"))
 
 GetMacOSStatusCommentString = Foundation.GetMacOSStatusCommentString
 GetMacOSStatusCommentString.restype = c_char_p
@@ -28,21 +29,25 @@ kFSFileOperationSkipSourcePermissionErrors = 0x02
 kFSFileOperationDoNotMoveAcrossVolumes = 0x04
 kFSFileOperationSkipPreflight = 0x08
 
+
 class FSRef(Structure):
-    _fields_ = [('hidden', c_char * 80)]
+    _fields_ = [("hidden", c_char * 80)]
+
 
 def check_op_result(op_result):
     if op_result:
-        msg = GetMacOSStatusCommentString(op_result).decode('utf-8')
+        msg = GetMacOSStatusCommentString(op_result).decode("utf-8")
         raise OSError(msg)
 
-def send2trash(path):
-    if not isinstance(path, binary_type):
-        path = path.encode('utf-8')
-    fp = FSRef()
-    opts = kFSPathMakeRefDoNotFollowLeafSymlink
-    op_result = FSPathMakeRefWithOptions(path, opts, byref(fp), None)
-    check_op_result(op_result)
-    opts = kFSFileOperationDefaultOptions
-    op_result = FSMoveObjectToTrashSync(byref(fp), None, opts)
-    check_op_result(op_result)
+
+def send2trash(paths):
+    paths = preprocess_paths(paths)
+    paths = [path.encode("utf-8") if not isinstance(path, binary_type) else path for path in paths]
+    for path in paths:
+        fp = FSRef()
+        opts = kFSPathMakeRefDoNotFollowLeafSymlink
+        op_result = FSPathMakeRefWithOptions(path, opts, byref(fp), None)
+        check_op_result(op_result)
+        opts = kFSFileOperationDefaultOptions
+        op_result = FSMoveObjectToTrashSync(byref(fp), None, opts)
+        check_op_result(op_result)
